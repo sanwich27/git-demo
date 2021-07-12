603a44ce87bfad47cbf3deb7f1c9e06cabea1c93
Xu-You Liu <kangarooonetwo@gmail.com>
2021-07-12 21:48:49 +0800

    Add python3 shebang to division.py

--- division.py
+++ division.py
@@ -1,3 +1,10 @@
-n = int(input("Enter numerator: "))
-d = int(input("Enter denominator: "))
+#!/usr/bin/env python3
+def num(n):
+    try:
+        return int(n)
+    except ValueError:
+        return float(n)
+
+n = num(input("Enter numerator: "))
+d = num(input("Enter denominator: "))
 print(n/d)
